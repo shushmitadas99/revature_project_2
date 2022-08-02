4c77c054f5177fe119fca2dfb5b02e571c553b3d
Jace Lenon <jace.lenon@yahoo.com>
2022-08-02 16:11:34 -0400

    Update sql script with inserted values

--- Project_2/SQL script Project 2.sql
+++ Project_2/SQL script Project 2.sql
@@ -1,23 +1,33 @@
+DROP TABLE IF EXISTS assignments;
+DROP TABLE IF EXISTS courses;
 DROP TABLE IF EXISTS teachers;
 DROP TABLE IF EXISTS students;
-DROP TABLE IF EXISTS courses;
-DROP TABLE IF EXISTS assignments;
 
 CREATE TABLE students (
 	s_id SERIAL PRIMARY KEY,
-	username VARCHAR(20) NOT NULL,
+	username VARCHAR(20) NOT NULL UNIQUE,
 	password VARCHAR(20) NOT NULL,
 	s_name VARCHAR(200) NOT NULL,
-	s_email	VARCHAR(200) NOT NULL
+	s_email	VARCHAR(200) NOT NULL UNIQUE
 );
+
+INSERT INTO students (username, password, s_name, s_email)
+VALUES
+('student1', 'password1', 'Student One', 'student1@email.com'), 
+('student2', 'password2', 'Student Two', 'student2@email.com');
 
 CREATE TABLE teachers (
 	t_id SERIAL PRIMARY KEY,
-	username VARCHAR(20) NOT NULL,
+	username VARCHAR(20) NOT NULL UNIQUE,
 	password VARCHAR(20) NOT NULL,
 	t_name VARCHAR(200) NOT NULL,
-	t_email	VARCHAR(200) NOT NULL
+	t_email	VARCHAR(200) NOT NULL UNIQUE
 );
+
+INSERT INTO teachers (username, password, t_name, t_email)
+VALUES
+('teacher1', 'password1', 'Teacher One', 'teacher1@email.com'),
+('teacher2', 'password2', 'Teacher Two', 'teacher2@email.com');
 
 CREATE TABLE courses (
 	c_id SERIAL PRIMARY KEY,
@@ -29,16 +39,22 @@ CREATE TABLE courses (
 	CONSTRAINT fk_t_id FOREIGN KEY (t_id) REFERENCES teachers(t_id)
 );
 
+INSERT INTO courses (c_name, c_desc, s_id, t_id)
+VALUES
+('science', 'chemistry', '1', '1'),
+('science', 'chemistry', '2', '1'),
+('math', 'algebra', '2', '2');
+
 CREATE TABLE assignments (
 	assn SERIAL PRIMARY KEY,
 	c_id INT NOT NULL,
     submitted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    grade CHAR NOT NULL,
+    grade VARCHAR(4),
     grade_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_c_id FOREIGN KEY (c_id) REFERENCES courses(c_id)
 );
 
-
---CREATE TABLE teachers (
---	
---)
+INSERT INTO assignments (c_id, grade)
+VALUES
+('1', NULL),
+('2', NULL);
