0fd2332a447f5e71d47c90cbb439192d86925d51
Siju Abraham <sijumariyam@msn.com>
2022-08-08 18:53:27 -0400

    add the assignments

--- Project_2/proj2_the_grading_mindset/dao/assignment_dao.py
+++ Project_2/proj2_the_grading_mindset/dao/assignment_dao.py
@@ -1,23 +1,84 @@
+from datetime import datetime, timezone
+
 import psycopg
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
-from model.assignment_model import Assignment
+import datetime
 
+from model.assignment_model import Assignments, Sassignments
 
 class AssignmentDao:
-    def get_all_assignments_by_username_and_course_id(self, course_id):
-        with psycopg.connect(host=config['host'], port=config['port'], dname=config['dbname'],
+
+         # dt = datetime.now(timezone.utc)
+    current_time = datetime.datetime.now()
+  # print(dt1)
+    print(current_time)
+
+    def get_all_assignments_by_s_id(self, s_id):
+        with psycopg.connect(host=config['host'], port=config['port'], dbname=config['dbname'],
                              user=config['user'], password=config['password']) as conn:
-           with conn.cursor() as cur:
-               cur.execute("select assignments.assn,assignments.submitted,assignments.grade,assignments.grade_time,"
-                           "courses.c_name,courses.c_desc,courses.s_id from assignments  join courses"
-                           " on assignments.c_id=courses.c_id and courses.s_id in(select s_id from courses"
-                           "  where courses.c_id = %s);", (course_id, ));
+            with conn.cursor() as cur:
 
-               assignment_list = []
+                cur.execute("select a.assn,a.submitted,a.grade,a.grade_time,c.c_name from assignments a join courses c on a.c_id = c.c_id and c.s_id in (select s_id from courses  where courses.s_id = %s)", (s_id, ))
 
-               for row in cur:
-                assignment_list.append(Assignment(row[0], row[1], row[2], row[3]))
+                assignment_list = []
+
+                for row in cur:
+
+                    my_assignment_obj = Sassignments(row[0], row[1], row[2], row[3], row[4])
+                    assignment_list.append(my_assignment_obj)
 
                 return assignment_list
+
+    def get_all_assignments_by_c_id(self, s_id, c_id):
+        with psycopg.connect(host=config['host'], port=config['port'], dbname=config['dbname'],
+                                     user=config['user'], password=config['password']) as conn:
+            with conn.cursor() as cur:
+                cur.execute("select a.assn, a.submitted, a.grade, a.grade_time, c.c_name, c.c_desc, c.s_id from assignments a join courses c on a.c_id = c.c_id and c.c_id in(select c_id from courses  where courses.c_id = %s and courses.s_id = %s)", (c_id, s_id))
+
+                assignment_list = []
+
+                for row in cur:
+
+                    my_assignment_obj = Sassignments(row[0], row[1], row[2], row[3], row[4])
+                    assignment_list.append(my_assignment_obj)
+
+                return assignment_list
+
+    def add_assignments_to_c_id(self, s_id, c_id, c_object):
+        with psycopg.connect(host=config['host'], port=config['port'], dbname=config['dbname'], user=config['user'],
+                             password=config['password']) as conn:
+
+            with conn.cursor() as cur:
+                cur.execute("select * from courses where s_id = %s and c_id = %s", (s_id, c_id))
+                assignments_new = cur.fetchone()
+                if assignments_new is None:
+                    return None
+                else:
+
+                    cur.execute("INSERT INTO assignments (c_id, grade) VALUES (%s, %s) RETURNING *", (c_id, c_object.grade))
+
+                assignment_row_inserted = cur.fetchone()
+                conn.commit()
+
+                return Assignments(assignment_row_inserted[0], assignment_row_inserted[1], assignment_row_inserted[2],
+                                     assignment_row_inserted[3], assignment_row_inserted[4])
+
+    def update_assignments_by_c_id_and_a_id(self, t_id, c_id, a_id,  a_object):
+        with psycopg.connect(host=config['host'], port=config['port'], dbname=config['dbname'], user=config['user'],
+                             password=config['password']) as conn:
+            current_time = datetime.datetime.now()
+            print(a_object.grade)
+            # print(current_time)
+            with conn.cursor() as cur:
+                cur.execute("update assignments set grade = %s, grade_time = %s where assn = %s and c_id = %s returning *", (a_object.grade, current_time, a_id, c_id))
+
+                conn.commit()
+
+                a_row_updated = cur.fetchone()
+                if a_row_updated is None:
+                    return None
+
+                return Assignments(a_row_updated[0], a_row_updated[1], a_row_updated[2], a_row_updated[3],
+                             a_row_updated[4])
--- Project_2/proj2_the_grading_mindset/service/assignment_service.py
+++ Project_2/proj2_the_grading_mindset/service/assignment_service.py
@@ -1,16 +1,58 @@
 from dao.assignment_dao import AssignmentDao
+from dao.student_dao import StudentDao
+from exception.course_errors import CourseNotFoundError
+from exception.student_errors import StudentNotFoundError
+from exception.student_errors import StudentNotFoundError
 
 
 class AssignmentService:
 
     def __init__(self):
-        self.assignment_dao = AssignmentDao
+        self.assignment_dao = AssignmentDao()
+        self.student_dao = StudentDao()
 
-    def get_all_assignments_by_username_and_course_id(self, course_id):
-        list_of__assignment_object = self.assignment_dao.get_all_assignments_by_username_and_course_id(course_id, )
+    def get_all_assignments_by_s_id(self, s_id):
+
+        # if self.assignment_dao.get_all_assignments_by_s_id(s_id) is None:
+        #     raise StudentNotFoundError()
+
+        list_of_assignment_object = self.assignment_dao.get_all_assignments_by_s_id(s_id)
+        if not list_of_assignment_object:
+            raise StudentNotFoundError()
 
         list_of_assignment_dictionaries = []
-        for assignment_obj in list_of__assignment_object:
-            list_of_assignment_dictionaries.append(assignment_obj.dict())
+
+        for assignment_obj in list_of_assignment_object:
+            list_of_assignment_dictionaries.append(assignment_obj.to_dict())
 
         return list_of_assignment_dictionaries
+
+    def get_all_assignments_by_c_id(self, s_id, c_id):
+        list_of_assignment_object = self.assignment_dao.get_all_assignments_by_c_id(s_id, c_id)
+        if not list_of_assignment_object:
+            raise CourseNotFoundError(f"Course with course_id {c_id} was not found")
+
+        list_of_assignment_dictionaries = []
+
+        for assignment_obj in list_of_assignment_object:
+            list_of_assignment_dictionaries.append(assignment_obj.to_dict())
+
+        return list_of_assignment_dictionaries
+
+    def add_assignments_to_c_id(self, s_id, c_id, c_object):
+        if self.student_dao.get_s_by_id(s_id) is None:
+            raise StudentNotFoundError(f"Student with id {s_id} was not found")
+
+        added_new_assignment = self.assignment_dao.add_assignments_to_c_id(s_id, c_id, c_object)
+        if not added_new_assignment:
+            raise CourseNotFoundError(f"Student with course_id{c_id} was not found")
+
+        return added_new_assignment.to_dict()
+    def update_assignments_by_c_id_and_a_id(self, t_id, c_id, a_id, a_object):
+        updated_assignment_object = self.assignment_dao.update_assignments_by_c_id_and_a_id(t_id, c_id, a_id, a_object)
+        # if updated_assignment_object is None:
+        #     raise CourseNotFoundError(f"Course with {c_object.id} was not found")
+        if updated_assignment_object is None:
+            raise CourseNotFoundError(f"Course with c_id {c_id} and assignment with a_id {a_id} was not found")
+
+        return updated_assignment_object.to_dict()
